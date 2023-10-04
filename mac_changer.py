03fd79aea43f9a2c6dc4c71c9f2873cf8c410c46
PCnslt <info@patriotconsultancy.com>
2023-10-04 07:14:49 -0400

    Create functions for  get_arguments and change_mac

--- mac_changer.py
+++ mac_changer.py
@@ -3,24 +3,28 @@
 import subprocess
 import optparse
 
-parser = optparse.OptionParser()
-parser.add_option("-i","--interface", dest="interface",help="Name of interface whose MAC address is to be changed.")
-parser.add_option("-m","--mac", dest="new_mac",help="New MAC address.")
-(options,arguments) = parser.parse_args()
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i","--interface", dest="interface",help="Name of interface whose MAC address is to be changed.")
+    parser.add_option("-m","--mac", dest="new_mac",help="New MAC address.")
+    return parser.parse_args()
 
-interface = options.interface #input("interface>")
-new_mac = options.new_mac #input ("new MAC>")
+def change_mac(interface, new_mac):    
+    ##One way of using 'subprocess' module
+    # subprocess.call("ifconfig",shell=True)
+    # subprocess.call("ifconfig "+interface+" down",shell=True)
+    # subprocess.call("ifconfig "+interface+" hw ether "+new_mac,shell=True)
+    # subprocess.call("ifconfig "+interface+" up",shell=True)
+    # subprocess.call("ifconfig "+interface,shell=True)
 
-##One way of using 'subprocess' module
-# subprocess.call("ifconfig",shell=True)
-# subprocess.call("ifconfig "+interface+" down",shell=True)
-# subprocess.call("ifconfig "+interface+" hw ether "+new_mac,shell=True)
-# subprocess.call("ifconfig "+interface+" up",shell=True)
-# subprocess.call("ifconfig "+interface,shell=True)
+    ##Another way of using 'subprocess module. Check documentation.
+    subprocess.call(["ifconfig"])
+    subprocess.call(["ifconfig",interface,"down"])
+    subprocess.call(["ifconfig",interface,"hw","ether"+new_mac])
+    subprocess.call(["ifconfig",interface,"up"])
+    subprocess.call(["ifconfig "+interface])
+    
 
-##Another way of using 'subprocess module. Check documentation.
-subprocess.call(["ifconfig"])
-subprocess.call(["ifconfig",interface,"down"])
-subprocess.call(["ifconfig",interface,"hw","ether"+new_mac])
-subprocess.call(["ifconfig",interface,"up"])
-subprocess.call(["ifconfig "+interface])
+(options,arguments) = get_arguments()
+
+change_mac(options.interface, options.new_mac)
