af11d29a564ccf427df983546b026299096167bc
PCnslt <info@patriotconsultancy.com>
2023-10-04 06:53:04 -0400

    Add command line argument capability to mac changer.

--- mac_changer.py
+++ mac_changer.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 
 import subprocess
+import optparse
 
-interface = input("interface>")
-new_mac = input ("new MAC>")
+parser = optparse.OptionParser()
+parser.add_option("-i","--interface", dest="interface",help="Name of interface whose MAC address is to be changed.")
+parser.add_option("-m","--mac", dest="new_mac",help="New MAC address.")
+(options,arguments) = parser.parse_args()
+
+interface = options.interface #input("interface>")
+new_mac = options.new_mac #input ("new MAC>")
 
 ##One way of using 'subprocess' module
 # subprocess.call("ifconfig",shell=True)
