506a1cffb48a9793879f880084a5294525365604
PCnslt <info@patriotconsultancy.com>
2023-10-04 06:31:25 -0400

    Add example of 'subprocess' module.  Check documentation

--- mac_changer.py
+++ mac_changer.py
@@ -5,8 +5,16 @@ import subprocess
 interface = input("interface>")
 new_mac = input ("new MAC>")
 
-subprocess.call("ifconfig",shell=True)
-subprocess.call("ifconfig "+interface+" down",shell=True)
-subprocess.call("ifconfig "+interface+" hw ether "+new_mac,shell=True)
-subprocess.call("ifconfig "+interface+" up",shell=True)
-subprocess.call("ifconfig "+interface,shell=True)
+##One way of using 'subprocess' module
+# subprocess.call("ifconfig",shell=True)
+# subprocess.call("ifconfig "+interface+" down",shell=True)
+# subprocess.call("ifconfig "+interface+" hw ether "+new_mac,shell=True)
+# subprocess.call("ifconfig "+interface+" up",shell=True)
+# subprocess.call("ifconfig "+interface,shell=True)
+
+##Another way of using 'subprocess module. Check documentation.
+subprocess.call(["ifconfig"])
+subprocess.call(["ifconfig",interface,"down"])
+subprocess.call(["ifconfig",interface,"hw","ether"+new_mac])
+subprocess.call(["ifconfig",interface,"up"])
+subprocess.call(["ifconfig "+interface])
