3b33d3331f5b4199dac5fada32f6b4561f8264c4
PCnslt <info@patriotconsultancy.com>
2023-10-06 22:25:14 -0400

    Add function to create ARP packet.

--- network_scanner.py
+++ network_scanner.py
@@ -3,6 +3,16 @@
 import scapy.all as scapy
 
 def scan(ip):
-    scapy.arping(ip)
+    
+    # scapy.arping(ip)
+    # scapy.ls(scapy.ARP())
+    arp_request = scapy.ARP(pdst = ip)
+    # arp_request.pdst = ip
+    arp_request.show()
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    broadcast.show()
+    arp_request_broadcast = broadcast/arp_request
+    print(arp_request_broadcast.summary())
+    arp_request_broadcast.show()
 
 scan("192.168.235.1/24")
