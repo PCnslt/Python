83e2e167f1056f36d8e0899941959a6de0447ede
PCnslt <info@patriotconsultancy.com>
2023-10-06 22:46:29 -0400

    Add final code that creates an ARP packet.

--- network_scanner.py
+++ network_scanner.py
@@ -2,17 +2,11 @@
 
 import scapy.all as scapy
 
-def scan(ip):
-    
-    # scapy.arping(ip)
-    # scapy.ls(scapy.ARP())
+def scan(ip):    
     arp_request = scapy.ARP(pdst = ip)
-    # arp_request.pdst = ip
-    arp_request.show()
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    broadcast.show()
     arp_request_broadcast = broadcast/arp_request
-    print(arp_request_broadcast.summary())
-    arp_request_broadcast.show()
+    answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1)
+    print(answered.summary())
 
 scan("192.168.235.1/24")
