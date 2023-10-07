29ac4d76ada47146268fb22fb9098f493028523a
PCnslt <info@patriotconsultancy.com>
2023-10-07 00:13:33 -0400

    Add options to CLI command. Used dictionaries and lists to enhance network scanner.

--- network_scanner.py
+++ network_scanner.py
@@ -1,16 +1,34 @@
 #!/usr/bin/env python
 
 import scapy.all as scapy
+import optparse
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-t","--target", dest="target",help="Exact/Range of target IP addresses.")
+    
+    (options,arguments) = parser.parse_args()
+    if not options.target:
+        parser.error("Please provide target IP address/range(1/24).")
+    return options
 
 def scan(ip):    
     arp_request = scapy.ARP(pdst = ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
     answered_list= scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
-    
-    print("IP\t\t\tMAC Address\n----------------------------")
-    
+        
+    clients_list =[]
     for element in answered_list:
-        print(element[1].psrc+"\t\t"+element[1].hwsrc)
+        clients_dict = {"ip":element[1].psrc,"mac":element[1].hwsrc}
+        clients_list.append(clients_dict)
+    return clients_list
 
-scan("192.168.235.1/24")
+def print_result(results_list):
+    print("IP\t\t\tMAC Address\n----------------------------")
+    for client in results_list:
+        print(client["ip"]+"\t\t"+client["mac"])
+
+options = get_arguments()
+scan_result = scan(options.target)
+print_result(scan_result)
